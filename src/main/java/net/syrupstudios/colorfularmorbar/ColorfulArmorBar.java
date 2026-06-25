f6ff9b482d210107c250b9de5ee6e04d10901b64
Salad <salad@bysalad.studio>
2026-06-25 19:06:44 +0200

    runs now

--- src/main/java/net/syrupstudios/colorfularmorbar/ColorfulArmorBar.java
+++ src/main/java/net/syrupstudios/colorfularmorbar/ColorfulArmorBar.java
@@ -1,16 +1,20 @@
 package net.syrupstudios.colorfularmorbar;
 
-import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.server.packs.PackType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ColorfulArmorBar implements ModInitializer {
+public class ColorfulArmorBar implements ClientModInitializer {
 	public static final String MOD_ID = "colorful_armor_bar";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
-		LOGGER.info("Colorful Armor Bar initialized!");
+	public void onInitializeClient() {
+		LOGGER.info("Colorful Armor Bar initialized (Client-Side)!");
+
+		ResourceManagerHelper.get(PackType.CLIENT_RESOURCES)
+				.registerReloadListener(new ArmorBarRegistry());
 	}
 }
--- src/main/java/net/syrupstudios/colorfularmorbar/mixin/InGameHudArmorMixin.java
+++ src/main/java/net/syrupstudios/colorfularmorbar/mixin/InGameHudArmorMixin.java
@@ -1,4 +1,4 @@
-package net.syrupstudios.colorfularmorbar.client.mixin;
+package net.syrupstudios.colorfularmorbar.mixin;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
@@ -7,7 +7,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
-import net.syrupstudios.colorfularmorbar.client.ArmorBarRegistry;
+import net.syrupstudios.colorfularmorbar.ArmorBarRegistry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
